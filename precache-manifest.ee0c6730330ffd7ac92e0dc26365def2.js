510222fd01c97137d6e5b5d15edc0a6763ee077f
amilbcahat <68186313+amilbcahat@users.noreply.github.com>
2022-02-04 02:50:13 +0530

    Updates

---
+++ precache-manifest.ee0c6730330ffd7ac92e0dc26365def2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2033cad9bdd73ede8363ea09e535bb",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b3024b54480214ce4c0e",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/my-app/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "bd81e5c6feaf63e9a711",
+    "url": "/my-app/static/js/3.4183067e.chunk.js"
+  },
+  {
+    "revision": "b3024b54480214ce4c0e",
+    "url": "/my-app/static/js/main.388d7ff1.chunk.js"
+  },
+  {
+    "revision": "500e28b52d3f1598c7c3",
+    "url": "/my-app/static/js/runtime~main.aa239d3e.js"
+  }
+]);
